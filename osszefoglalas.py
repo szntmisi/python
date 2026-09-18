67874b0b6b15e9788d4e1de1863728c77e961301
szntmisi <szantomihaly48@gmail.com>
2026-09-18 12:08:46 +0200

    valami

---
+++ osszefoglalas.py
@@ -0,0 +1,10 @@
+#1. adat tipusok és konverziok
+#   szotar.str.int.float.boolean,halmaz,topple
+#2. vezérlési struktúrák
+#   if,elif,else,while.for
+#3.összegzés tétel(bejárás index és elem alapján)
+#4. megszámlálás tétel(for,vagy index alapján)
+#5. átlagolás(páros számok átlaga)
+#eldüntés tétel(while vagy for {van})
+#7. kiválasztés tétel
+#8.keresés tétel()
--- prog_tetelek.py
+++ prog_tetelek.py
@@ -14,43 +14,88 @@ szamok = [5, 6, 7]
 
 # print(osszeg)
 #összegzés függvénnyel
-def osszegzes(lista):
-    """összeadja a listá-ban lévő számokat"""
-    osszeg = 0
-    for szam in lista:
-        osszeg += szam
+# def osszegzes(lista):-----------------------------------------------------------------------------------
+#     """összeadja a listá-ban lévő számokat"""
+#     osszeg = 0
+#     for szam in lista:
+#         osszeg += szam
 
-    return osszeg
+#     return osszeg
 
-print(osszegzes(szamok))
-print(sum(szamok))
-#add meg a szémok átlagát!
-szamok = [5, 5, 7]
-print(f"a számok átlaga:{osszegzes(szamok) / len(szamok):.2f}")
+# print(osszegzes(szamok))
+# print(sum(szamok))
+# #add meg a szémok átlagát!
+# szamok = [5, 5, 7]
+# print(f"a számok átlaga:{osszegzes(szamok) / len(szamok):.2f}")
 
-from statistics import mean 
-print(mean(szamok))
+# from statistics import mean 
+# print(mean(szamok))
 
-#megszámolás hány darab páros szám van a megadott számok között
-szamok = [5, 6, 7, 8]
+# #megszámolás hány darab páros szám van a megadott számok között
+# szamok = [5, 6, 7, 8]
 
-db = 0 
+# db = 0 
+# for szam in szamok:
+#     if szam % 2 == 0  :
+#         db +=1
+
+# print(f"páros számok : {db}.")
+
+
+
+# #gyakorló feladat add meg a páros számok átlagát
+# paros_db = 0
+# szamok = [5, 6, 7, 8, 3, 2, 5, 4, 9]
+# paros_osszeg = 0
+# for szam in szamok:
+#     if szam % 2 == 0:
+#         paros_db += 1
+#         paros_osszeg += szam
+
+# print(f"páros számok átlaga: {paros_osszeg / paros_db:.2f}.")--------------------------------------------
+#eldöntés
+# lehetséges válaaszok:van,nincs,mind ilyen,egy ilyen sincs 
+#visszatérési érték az egy logikai érték
+#pl: van e páros szám a listában?,a listában minden szám páros?...
+#kérdés be kell a járni a listát?
+#van e páros szám a listában?
+szamok = [ 4,5, 7, 9,]
+
+i = 0
+while i < len(szamok) and not(szamok[i] %2 == 0):
+    i += 1
+van = i < len(szamok)
+print(f"{'van'if van else  'nincs'} páros szám a listában")
+
+#minden szám páratlan e?
+szamok = [5, 4, 7, 9,]
+
+i = 0
+while i < len(szamok) and not(szamok[i] %2 == 0):
+    i += 1
+
+van = i < len(szamok)
+print(f"{'Nem minden'if van else 'Minden'} szám páratlan")
+
+#eldöntés  V2
+#van e páros szám a listában
+van = False 
+
 for szam in szamok:
-    if szam % 2 == 0  :
-        db +=1
+    if szam %2 == 0:
+        van = True
+        break
 
-print(f"páros számok : {db}.")
+print(f"{'van'if van else  'nincs'} páros szám a listában")
 
+#kiválasztás tétele
+#ha biztosan tudjuk hogy van olyan elem akkor adjuk meg a sorszámot
+#vissza térési érték: egy sorszám,ami a addot tulajdonsága elem a listában
+#példa: hanyadik ember a legmagasabba listában
+# hanyadik elem az első páros szám 
+szamok = [4, 7, 5, 9,]
+i = 0
+while not (szamok[i] %2 == 0):
+    i += 1
 
-
-#gyakorló feladat add meg a páros számok átlagát
-paros_db = 0
-szamok = [5, 6, 7, 8, 3, 2, 5, 4, 9]
-paros_osszeg = 0
-for szam in szamok:
-    if szam % 2 == 0:
-        paros_db += 1
-        paros_osszeg += szam
-
-print(f"páros számok átlaga: {paros_osszeg / paros_db:.2f}.")
-    
+print(f"az első páros elem indexe a(z):{i},értéke {szamok[i]}")
